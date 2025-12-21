aff806d3a6eaf5606d654e1d25a5f3ad6717813d
sedounet <dev@dotlyn.local>
2025-12-21 14:25:01 +0100

    [dotlyn_core] fix: remove exports of deleted services

--- packages/dotlyn_core/lib/dotlyn_core.dart
+++ packages/dotlyn_core/lib/dotlyn_core.dart
@@ -2,5 +2,3 @@ library dotlyn_core;
 
 export 'utils/constants.dart';
 export 'providers/theme_provider.dart';
-export 'services/vibration_service.dart';
-export 'services/notification_service.dart';
